65d69e5d428e92a8a120c704c3cc46e9f827e405
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-09-15 18:26:57 +0700

    Fix scene file path in play button press function

--- scenes/start_scene/start_scene.gd
+++ scenes/start_scene/start_scene.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_play_btn_pressed() -> void:
-  get_tree().change_scene_to_file("res://scenes/play_1st_scene/play_1st_scene.tscn")
+  get_tree().change_scene_to_file("res://scenes/play_scene_1/play_1st_scene.tscn")
 
 
 func _on_quit_btn_pressed() -> void:
